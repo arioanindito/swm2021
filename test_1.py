4177faa45a3ec1bcd1e468c79250ab80b792c8dd
arioanindito <72435123+arioanindito@users.noreply.github.com>
2021-11-23 20:00:44 +0200

    The Improve Code
    Make an improvement based on pylint

--- test_1.py
+++ test_1.py
@@ -1,19 +1,21 @@
+"""System module."""
 from time import strftime
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
-
-class clockApp(App):
+class ClockApp(App):
+    """Return the pathname of the KOS root directory."""
     sw_started= False
     sw_seconds = 0
     def update_time(self, nap):
+        """Dummy Docstring"""
         if self.sw_started:
             self.sw_seconds += nap
         minutes, seconds = divmod(self.sw_seconds, 60)
         self.root.ids.stopwatch.text = (
-            '%02d:%02d.[size=40]%02d[/size]'%
+            '%02d:%02d.[size=40]%02d[/size]'% #pylint: disable=consider-using-f-string
             (int(minutes), int(seconds),
              int(seconds* 100 % 100))
         )
@@ -21,11 +23,13 @@ class clockApp(App):
     def on_start(self):
         Clock.schedule_interval(self.update_time, 0)
     def start_stop(self):
+        """Dummy Docstring"""
         self.root.ids.start_stop.text =(
             'Start' if self.sw_started else 'Stop'
         )
         self.sw_started = not self.sw_started
     def reset(self):
+        """Dummy Docstring"""
         if self.sw_started:
             self.root.ids.start_stop.text = 'Start'
             self.sw_started = False
@@ -38,4 +42,4 @@ if __name__ == '__main__':
         fn_regular= 'Roboto-Thin.ttf',
         fn_bold= 'Roboto-Medium.ttf'
     )
-    clockApp().run()
+    ClockApp().run()
